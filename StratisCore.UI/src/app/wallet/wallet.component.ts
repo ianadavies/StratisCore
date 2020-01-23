b8f4f545c7e3c03e942237a2c72b78ad9c7d0911
Ian Davies <ian_a_davies@hotmail.com>
2020-01-23 19:20:28 +0200

    Start Applying Theme to Cirrus Core 2

--- StratisCore.UI/src/app/wallet/wallet.component.ts
+++ StratisCore.UI/src/app/wallet/wallet.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { LogoutConfirmationComponent } from "./logout-confirmation/logout-confirmation.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LogoutConfirmationComponent } from './logout-confirmation/logout-confirmation.component';
 import { TaskBarService } from '@shared/services/task-bar-service';
+import { GlobalService } from '@shared/services/global.service';
 
 @Component({
   selector: 'wallet-component',
@@ -9,9 +10,13 @@ import { TaskBarService } from '@shared/services/task-bar-service';
   styleUrls: ['./wallet.component.scss'],
 })
 export class WalletComponent {
+  sidechainEnabled: any;
+
   constructor(
+    private globalService: GlobalService,
     private taskBarService: TaskBarService,
     private modalService: NgbModal) {
+    this.sidechainEnabled = globalService.getSidechainEnabled();
   }
 
   public logout(): void {
